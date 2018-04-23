a19e0a38781d26db3c330cb94f1e941cdd5f927e
Santiago Ferreira <san650@gmail.com>
2018-04-23 20:24:58 -0300

    Update package metadata

--- mix.exs
+++ mix.exs
@@ -4,10 +4,13 @@ defmodule HtmlParty.Mixfile do
   def project do
     [
       app: :html_party,
-      version: "0.1.0",
+      description: description(),
+      version: "1.0.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: "https://github.com/san650/html_party",
     ]
   end
 
@@ -23,6 +26,22 @@ defmodule HtmlParty.Mixfile do
     [
       # Test dependencies
       {:phoenix_html, "~> 2.11", only: :test}
+    ]
+  end
+
+  defp description do
+    """
+    This library helps you write HTML using regular functions. It provides a small DSL that ensures that you write safe HTML.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/san650/html_party"},
+      maintainers: ["Santiago Ferreira"],
+      name: :html_party,
     ]
   end
 end
